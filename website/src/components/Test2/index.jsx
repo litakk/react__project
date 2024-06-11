8ca0303c4aa7fd552bcbe191b91c239b754b56d3
Feruz <140828663+litakk@users.noreply.github.com>
2024-06-11 23:04:05 +0500

    updated

---
+++ website/src/components/Test2/index.jsx
@@ -0,0 +1,15 @@
+const style = {
+    backgroundColor: "black",
+    color: "red",
+}
+
+function Test2() {
+    return ( 
+<div className="test">
+    <h1 style={style}>Test 2</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ipsa alias minus natus modi exercitationem necessitatibus asperiores voluptatibus, unde corrupti quam ipsum nobis qui dicta, nihil illum repudiandae aperiam dignissimos.</p>
+</div>        
+     );
+}
+
+export default Test2;
