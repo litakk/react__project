28001aaae6aa7f43d6596007470d6c83efbbf683
Feruz <140828663+litakk@users.noreply.github.com>
2024-06-12 23:14:28 +0500

    up

--- website/src/components/Test.jsx
+++ website/src/components/Test.jsx
@@ -8,6 +8,7 @@ function Test() {
 <div className="test">
     <h1 style={style}>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ipsa alias minus natus modi exercitationem necessitatibus asperiores voluptatibus, unde corrupti quam ipsum nobis qui dicta, nihil illum repudiandae aperiam dignissimos.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, ipsa alias minus natus modi exercitationem necessitatibus asperiores voluptatibus, unde corrupti quam ipsum nobis qui dicta, nihil illum repudiandae aperiam dignissimos.</p>
 </div>        
      );
 }
