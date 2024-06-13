f8a0f04c12faef4317a3746ee42fb07707872f6d
Feruz <140828663+litakk@users.noreply.github.com>
2024-06-14 01:51:58 +0500

    up

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './components/navigation/Navbar'
+import LandingPage from './components/LandingPage'
 
 function App() {
 
   return (
     <main className="app">
       <Navbar />
+      <LandingPage />
     </main>
   )
 }
--- website/src/components/common/Heading/index.jsx
+++ website/src/components/common/Heading/index.jsx
@@ -1,17 +1,20 @@
 // props: properties
 
-import HeadingVector from "../../../assets/icons/heading-vector.png" 
+import HeadingVector from "../../../assets/icons/heading-vector.png"
 import "./style.scss"
 
 function Heading(props) {
+    const desireSize = props.size || 1
+    const fontSize = (48 / desireSize) + "px"
+
     // { title: "...", children: "..."}
-    return ( 
+    return (
         <div className="heading-wrapper">
             <img src={HeadingVector} alt="Heading Vector" />
-            <h1>{ props.title }</h1>
+            <h1 style={{ fontSize: fontSize }}> {props.title}</h1>
             {/* <p>{ props.children }</p> */}
         </div>
-     );
+    );
 }
 
 export default Heading;
