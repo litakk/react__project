a0c57d6f322b60e3b94eeae03d4a72d18de25492
Feruz <140828663+litakk@users.noreply.github.com>
2024-06-29 15:56:14 +0500

    update

--- website/src/components/navigation/Navbar/index.jsx
+++ website/src/components/navigation/Navbar/index.jsx
@@ -1,9 +1,21 @@
-import Heading from "../../common/Heading"
-import './style.scss'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // добавлен импорт Link из react-router-dom
+import Heading from '../../common/Heading'; // исправлен путь к Heading
+import './Modal.css'; // исправлен импорт стилей для модального окна
 
 function Navbar(props) {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    // Функция для открытия модального окна
+    const openModal = () => {
+        setModalOpen(true);
+    };
+
+    // Функция для закрытия модального окна
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
     return (
         <nav className="navbar-wrapper">
             <Link to="/">
@@ -19,13 +31,33 @@ function Navbar(props) {
             </div>
 
             <div className="auth-section">
-                <button className='btn btn-warning'>
+                <button className='btn btn-warning' onClick={openModal}>
                     Войти
                 </button>
                 <a href="#">
                     <ins>Рус</ins>
                 </a>
             </div>
+
+            {/* Модальное окно */}
+            {modalOpen && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <span className="modal-close" onClick={closeModal}>Закрыть</span>
+                        <h2>Авторизация или Регистрация</h2>
+                        {/* Здесь может быть форма для авторизации или регистрации */}
+                        <form>
+                            <label htmlFor="username">Имя пользователя:</label>
+                            <input type="text" id="username" name="username" />
+
+                            <label htmlFor="password">Пароль:</label>
+                            <input type="password" id="password" name="password" />
+
+                            <button type="submit">Войти</button>
+                        </form>
+                    </div>
+                </div>
+            )}
         </nav>
     );
 }
